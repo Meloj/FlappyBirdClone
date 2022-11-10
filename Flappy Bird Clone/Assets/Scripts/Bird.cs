973852f88b0a7cc5593d196be88560c0c7bbc2f0
João Paulo Melo <meloj0299@gmail.com>
2022-11-09 23:13:39 -0300

    Update Initial Project
    First commit of the clone. Not finished yet.

---
+++ Flappy Bird Clone/Assets/Scripts/Bird.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float speed = 1f;
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rig.velocity = Vector2.up * speed;
+        }
+    }
+}
---
+++ Flappy Bird Clone/Assets/Scripts/SpawnPipes.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPipes : MonoBehaviour
+{
+
+    public GameObject pipe;
+    public float height;
+    public float maxTime;
+    private float timer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newPipe = Instantiate(pipe);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+
+            Destroy(newPipe, 20f);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
